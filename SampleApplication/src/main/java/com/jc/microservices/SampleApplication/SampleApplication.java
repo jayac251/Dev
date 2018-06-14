b3f7cdbc2785febbd14f7cbd058234a187de3400
197651 <197651@PC369283.cts.com>
2018-06-14 11:27:28 +0530

    Working version

--- SampleApplication/src/main/java/com/jc/microservices/SampleApplication/SampleApplication.java
+++ SampleApplication/src/main/java/com/jc/microservices/SampleApplication/SampleApplication.java
@@ -1,12 +1,19 @@
 package com.jc.microservices.SampleApplication;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+		
+		
 	}
 }
