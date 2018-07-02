3cf5046590577b68a4874f3278a19d47673730bd
197651 <197651@PC369283.cts.com>
2018-07-02 19:21:10 +0530

    channel

--- spring-integration/src/main/java/com/example/demo/SpringIntegrationApplication.java
+++ spring-integration/src/main/java/com/example/demo/SpringIntegrationApplication.java
@@ -3,14 +3,18 @@ package com.example.demo;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 
@@ -21,6 +25,9 @@ import com.example.demo.print.PrintService;
 @ImportResource("integration-context.xml")
 public class SpringIntegrationApplication implements ApplicationRunner {
 
+	@Autowired
+	private DirectChannel channel1;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntegrationApplication.class, args);
 	}
@@ -36,6 +43,19 @@ public class SpringIntegrationApplication implements ApplicationRunner {
 		service.print(message);
 		
 		Message<String> message2=MessageBuilder.withPayload("Hello from Builder").setHeader("key2","value2").build();
-		service.print(message2);
+		//service.print(message2);
+		
+		channel1.subscribe(new MessageHandler(){
+
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				// TODO Auto-generated method stub
+				service.print((Message<String>) message);
+			}});
+		
+		channel1.send(message2);
+		
+		
+		
 	}
 }
