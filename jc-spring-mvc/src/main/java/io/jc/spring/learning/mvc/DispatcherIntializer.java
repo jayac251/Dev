53fc3478dd57f753451b177fcd23be05d6de4439
Dell <Dell@Dell-PC>
2020-04-09 23:03:15 +0530

    Expedited Dispatcher Coniguration
    Removed appInitializer. Instead configure through Dispatcher Initializer

---
+++ jc-spring-mvc/src/main/java/io/jc/spring/learning/mvc/DispatcherIntializer.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package io.jc.spring.learning.mvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Dell
+ *
+ */
+public class DispatcherIntializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getRootConfigClasses()
+	 */
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer#getServletConfigClasses()
+	 */
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {WebConfig.class};
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.support.AbstractDispatcherServletInitializer#getServletMappings()
+	 */
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
