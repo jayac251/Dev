e432b9760112a7158dcf6e4d6f0f56b7283959cd
Dell <Dell@Dell-PC>
2020-04-09 22:46:36 +0530

    Dispatcher Servlet Intro

--- jc-spring-mvc/src/main/java/io/jc/spring/learning/mvc/AppInitializer.java
+++ jc-spring-mvc/src/main/java/io/jc/spring/learning/mvc/AppInitializer.java
@@ -5,8 +5,11 @@ package io.jc.spring.learning.mvc;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * @author Dell
@@ -19,7 +22,14 @@ public class AppInitializer implements WebApplicationInitializer {
 	 */
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
-		System.out.println("HelloWorld");
+		AnnotationConfigWebApplicationContext dispatcherContext=new AnnotationConfigWebApplicationContext();
+		dispatcherContext.register(WebConfig.class);
+		
+		DispatcherServlet dispatcherServlet=new DispatcherServlet(dispatcherContext);
+		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", dispatcherServlet);
+		registration.addMapping("/");
+		registration.setLoadOnStartup(1);
+		
 	}
 
 }
---
+++ jc-spring-mvc/src/main/java/io/jc/spring/learning/mvc/WebConfig.java
@@ -0,0 +1,11 @@
+package io.jc.spring.learning.mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan("io.jc.spring.learning")
+public class WebConfig {
+	
+
+}
